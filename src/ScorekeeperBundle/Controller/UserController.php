4ed42abcf3971f393c267c669cb2d4f94884cf86
Rob Haverkort <rob.haverkort@ziggo.nl>
2015-04-07 17:46:23 +0200

    added user password change

--- src/ScorekeeperBundle/Controller/UserController.php
+++ src/ScorekeeperBundle/Controller/UserController.php
@@ -5,9 +5,10 @@ namespace ScorekeeperBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-
+use ScorekeeperBundle\Form\Type\UserType;
 
 class UserController extends Controller {
 
@@ -24,19 +25,50 @@ class UserController extends Controller {
 
     /**
      * @Route("/user/{user_id}", name="user_view")
-     * @Security("has_role('ROLE_USER')")
-     * XXX@Security("is_granted('user_view',user)")
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function viewAction($user_id) {
         $repository = $this->getDoctrine()
                 ->getRepository('ScorekeeperBundle:User');
         $user = $repository->find($user_id);
 
+        return $this->render('ScorekeeperBundle:User:view.html.twig', array('user' => $user));
+    }
+
+    /**
+     * @Route("/user/edit/{user_id}", name="user_edit")
+     * @Security("has_role('ROLE_USER')")
+     * XXX@Security("is_granted('user_view',user)")
+     */
+    public function editAction(Request $request, $user_id) {
+        $repository = $this->getDoctrine()
+                ->getRepository('ScorekeeperBundle:User');
+        $user = $repository->find($user_id);
+
         if (false === $this->get('security.authorization_checker')->isGranted('view', $user)) {
             throw new AccessDeniedException('Unauthorised access!');
-        }        
-        
-        return $this->render('ScorekeeperBundle:User:view.html.twig', array('user' => $user));
+        }
+
+        $form = $this->createForm(new UserType(), $user, array(
+            'action' => $this->generateUrl('user_edit', array('user_id' => $user->getId()))
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            if ($form['plainPassword']->getData() == $form['plainPassword2']->getData()) {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $form['plainPassword']->getData());
+                $user->setPassword($encoded);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('notice', 'nieuw password: ' . $encoded);
+            }
+            return $this->render('ScorekeeperBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+        }
+
+        return $this->render('ScorekeeperBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
     }
 
 }
---
+++ src/ScorekeeperBundle/Form/Type/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ScorekeeperBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('username', 'text',array('disabled'=>true))
+                ->add('name', 'text',array('disabled'=>true))
+                ->add('email', 'text',array('disabled'=>true))
+                ->add('isActive', 'checkbox',array('disabled'=>true))
+                ->add('plainPassword', 'text',array('mapped'=>false))
+                ->add('plainPassword2', 'text',array('mapped'=>false))
+                ->add('save', 'submit', array('label' => 'Submit'));
+    }
+
+    public function getName() {
+        return 'user';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'ScorekeeperBundle\Entity\User',
+        ));
+    }
+
+}
