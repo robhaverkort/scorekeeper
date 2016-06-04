069edde45ca3f06f8ee603055d7d9697c70d765b
Rob Haverkort <rob.haverkort@ziggo.nl>
2016-06-05 00:31:46 +0200

    some deprecations resoled

--- src/ScorekeeperBundle/Form/Type/ResultType.php
+++ src/ScorekeeperBundle/Form/Type/ResultType.php
@@ -12,22 +12,24 @@ class ResultType extends AbstractType {
 
         $builder
                 //->add('contest','entity',array('class'=>'ScorekeeperBundle:Contest','property'=>'id'))
-                ->add('user', 'entity', array('class' => 'ScorekeeperBundle:User', 'property' => 'name', 'placeholder' => ''))
+                ->add('user', 'Symfony\Bridge\Doctrine\Form\Type\EntityType', array('class' => 'ScorekeeperBundle:User', 'choice_label' => 'name', 'placeholder' => ''))
                 //->add('details', 'text')
-                ->add('s10', 'text', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
-                ->add('s09', 'text', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
-                ->add('s08', 'text', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
-                ->add('s07', 'text', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
-                ->add('s06', 'text', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
-                ->add('total', 'text', array('attr' => array('maxlength' => 3, 'size' => 4, 'onInput' => 'cleardetails();')))
-                ->add('save', 'submit', array('label' => 'Add'));
+                ->add('s10', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
+                ->add('s09', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
+                ->add('s08', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
+                ->add('s07', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
+                ->add('s06', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('mapped' => false, 'required' => false, 'attr' => array('maxlength' => 2, 'size' => 3, 'onInput' => 'calctotal();')))
+                ->add('total', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('attr' => array('maxlength' => 3, 'size' => 4, 'onInput' => 'cleardetails();')))
+                ->add('save', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', array('label' => 'Add'));
     }
 
     public function getName() {
         return 'result';
     }
 
+    
     public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    //public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'ScorekeeperBundle\Entity\Result',
         ));
