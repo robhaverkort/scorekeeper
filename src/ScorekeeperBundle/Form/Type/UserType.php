65b96d4beb24487fef331c22188ee6d622aa4e6f
Rob Haverkort <rob.haverkort@ziggo.nl>
2016-08-20 16:55:16 +0200

    some deprecations resoled

--- src/ScorekeeperBundle/Form/Type/UserType.php
+++ src/ScorekeeperBundle/Form/Type/UserType.php
@@ -5,25 +5,37 @@ namespace ScorekeeperBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class UserType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('username', 'Symfony\Component\Form\Extension\Core\Type\TextType',array('disabled'=>true))
-                ->add('name', 'Symfony\Component\Form\Extension\Core\Type\TextType',array('disabled'=>true))
-                ->add('email', 'Symfony\Component\Form\Extension\Core\Type\TextType',array('disabled'=>true))
-                ->add('isActive', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType',array('disabled'=>true))
-                ->add('plainPassword', 'Symfony\Component\Form\Extension\Core\Type\PasswordType',array('mapped'=>false))
-                ->add('plainPassword2', 'Symfony\Component\Form\Extension\Core\Type\PasswordType',array('mapped'=>false))
+                ->add('username', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('disabled' => true))
+                ->add('name', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('disabled' => true))
+                ->add('email', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('disabled' => true))
+                ->add('isActive', 'Symfony\Component\Form\Extension\Core\Type\CheckboxType', array('disabled' => true))
+                ->add('plainPassword', 'Symfony\Component\Form\Extension\Core\Type\PasswordType', array('mapped' => false))
+                ->add('plainPassword2', 'Symfony\Component\Form\Extension\Core\Type\PasswordType', array('mapped' => false))
                 ->add('save', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', array('label' => 'Submit'));
     }
 
-    public function getName() {
+//    public function getName() {
+//        return 'user';
+//    }
+
+    public function getBlockPrefix() {
         return 'user';
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+//    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+//        $resolver->setDefaults(array(
+//            'data_class' => 'ScorekeeperBundle\Entity\User',
+//        ));
+//    }
+
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'ScorekeeperBundle\Entity\User',
         ));
